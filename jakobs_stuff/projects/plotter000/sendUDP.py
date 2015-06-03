4fcb092b4296b7f14f72ab0e2c1515095536bfd1
jakobhm <j.wittmann@hm.edu>
2015-06-03 18:24:59 +0200

    blub

--- jakobs_stuff/projects/plotter000/sendUDP.py
+++ jakobs_stuff/projects/plotter000/sendUDP.py
@@ -8,7 +8,8 @@ Created on Wed Apr 15 10:45:56 2015
 import socket
 import time
 
-UDP_IP = "127.0.0.1"
+#UDP_IP = "127.0.0.1"
+UDP_IP = "10.27.192.152"
 UDP_PORT = 0xAAAA
 MESSAGE = "Hello, World!"
 
@@ -21,6 +22,7 @@ val=0
 while True:
     sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
     sock.sendto(str(val), (UDP_IP, UDP_PORT))
+    print "sende: ", val
     if val==20:
         val=1
     else:
--- jakobs_stuff/projects/plotter000/udpServer.py
+++ jakobs_stuff/projects/plotter000/udpServer.py
@@ -6,16 +6,19 @@ Created on Tue Jun 02 17:33:21 2015
 """
 
 import socket
+from struct import *
+from datetime import datetime
 
 #UDP_IP = "192.168.178.30"
 #UDP_IP = "169.254.207.255"
-UDP_IP = "192.168.178.21"
-UDP_PORT = 0xAAAA
+MY_UDP_IP = "10.27.192.90"
+MY_UDP_PORT = 60111
 
 sock = socket.socket(socket.AF_INET, # Internet
                  socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+sock.bind((MY_UDP_IP, MY_UDP_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+    dataRaw, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    data=unpack('!3B',dataRaw)
+    print ('at {} message from {}: {}'.format(datetime.now(), addr, data))
